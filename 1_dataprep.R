9d5a1e94951e2ad93ea214cb337c9e04748cec5c
elisehellwig <echellwig@ucdavis.edu>
2018-06-13 08:18:20 -0700

    started to code up the data importing step.

---
+++ 1_dataprep.R
@@ -0,0 +1,9 @@
+#######################Data Prep######################
+## this script imports all the data and processes it for analysis.
+
+###############Setup##############
+library(raster)
+library(rgdal)
+library(tidyverse)
+
+datapath <- '/Users/echellwig/Research/frogData/data'
