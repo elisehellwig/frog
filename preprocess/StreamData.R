9f6c2a411193480a2b28af8aee33341f635454af
elisehellwig <echellwig@ucdavis.edu>
2018-06-15 10:25:43 -0700

    just trying to get all the variables to be the same between the two stream shape files blech.

--- preprocess/StreamData.R
+++ preprocess/StreamData.R
@@ -1,10 +1,27 @@
 #######################Data Prep######################
-## this script imports all the data and processes it for analysis.
+## this script imports the stream and processes it for analysis.
 
 ###############Setup##############
 library(raster)
 library(rgdal)
 library(tidyverse)
 
-datapath <- '/Users/echellwig/Research/frogData/data'
+datapath <- '/Users/echellwig/Research/frogData/data/'
+
+ca <- shapefile(file.path(datapath, 'frog_model/NHDFlowline_Streams_Final.shp'))
+nv <- shapefile(file.path(datapath, 
+                          'frog_model/NHDFlowlineGB_Streams_Final.shp'))
+
+x <- read.table(file.path(datapath, 'cathy/rasi_mxt_allstr3.tab'), sep='\t',
+                header=TRUE)
+
+#########################################################
+
+names(ca) <- tolower(names(ca))
+names(nv) <- tolower(names(nv))
+
+names(ca) <- gsub(".*q","prcp_q", names(ca))
+names(nv) <- gsub(".*q","prcp_q", names(nv))
+
+
 
