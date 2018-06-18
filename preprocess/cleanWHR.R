2e30fe66bc2a4d6365747173c878d2b44af7ba64
elisehellwig <echellwig@ucdavis.edu>
2018-06-18 13:05:13 -0700

    in the process of exploring and cleaning the WHR data, lot of it is missing. not sure where we will get the rest.

--- preprocess/cleanWHR.R
+++ preprocess/cleanWHR.R
@@ -2,6 +2,58 @@
 
 library(raster)
 library(rgdal)
+library(rgeos)
 library(tidyverse)
+library(ehelpr)
+
+#setting paths
+funpath <- '/Users/echellwig/Research/frog/functions/'
+datapath <- '/Users/echellwig/Research/frogData/data/'
+
+#importing functions
+source(file.path(funpath, 'qc.R'))
+
+#lots of non overlap with stream
+cwhr <- shapefile(file.path(datapath, 'frog_model/data/output/ChwrMerge.shp'))
+
+#whrnames <- c("GBflowlineINTERSECTtnfcwhrpolygon.shp",
+            #  "NHDflowlineINTERSECTpnfcwhrpolygon.shp",
+            #  "NHDflowlineINTERSECTtnfcwhrpolygon.shp",
+            #  "lnfwhr.shp")
+
+#whrpaths <- paste('whr', whrnames, sep='/')
+#whrlist <- lapply(whrpaths, function(fn) shapefile(file.path(datapath, fn)))
+
+#whrall <- do.call(bind, whrlist)
+#whr <- aggregate(whrall)
+
+stream <- readRDS(file.path(datapath, 'processed/RasiStreamLines.RDS'))
+
+(NAs <- length(which(is.na(stream$whrtype))))
+
+
+TA <- CRS('+proj=aea +lat_1=34 +lat_2=40.5 +lat_0=0 +lon_0=-120 +x_0=0 +y_0=-4000000 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0 ')
+
+
+streamTA <- spTransform(stream, TA) 
+whrTA <- spTransform(cwhr, TA)
+
+
+whragg <- aggregate(whrTA)
+whrbuff <- gBuffer(whragg, byid=TRUE, width=0)
+
+
+outsideStreams <- gDifference(streamTA, whrbuff, byid=TRUE)
+
+plot(whragg, col='lightblue', main='Extent of Wildlife Habitat Relationship (WHR) Layers \n and Stream Locations, Red = ouside WHR extent')
+plot(streamTA, add=TRUE, col='navy')
+plot(outsideStreams, add=TRUE, col='red3')
+
+
+#extracting values to see now many NAs we have
+
+habitat <- overChr(streamTA, whrTA, 'WHRTYPE')
+
+countNA(habitat)
 
 
