dfda55871b4fbc77c5b0feb0a4259837352523c4
elisehellwig <echellwig@ucdavis.edu>
2018-06-18 13:04:47 -0700

    rewrote code to extract a character variable to use my newly written function overChr

--- preprocess/ProcessSoils.R
+++ preprocess/ProcessSoils.R
@@ -6,7 +6,12 @@ library(rgdal)
 library(rgeos)
 library(foreign)
 
+#setting paths
+funpath <- '/Users/echellwig/Research/frog/functions/'
 datapath <- '/Users/echellwig/Research/frogData/data/'
+
+#importing functions
+source(file.path(funpath, 'qc.R'))
 
 soil <- shapefile(file.path(datapath, 
                 'frog_model/data/output/MergedSoilsClipFinal.shp'))
@@ -16,19 +21,14 @@ ras <- readRDS(file.path(datapath, 'processed/RasiStreamLines.RDS'))
 #########################################################
 
 soill <- spTransform(soil, crs(ras))
-soilorder <- soill[,'SoilOrderD']
 
-soilagg <- aggregate(soill)
-geodif <- gDifference(ras, soilagg, byid=TRUE)
-
-rasSoil <- over(ras, soilorder, returnList = TRUE)
-
-rs <- sapply(rasSoil, function(rs) rs$SoilOrderD)
-rsNA <- sapply(rs, function(v) getMode(v))
-
-rasSoilvec <- sapply(rasSoil, function(rs) getMode(rs$SoilOrderD))
+rsv <- overChr(ras, soill, 'SoilOrderD')
 
 soil <- rbind(soilca[, varnames], soilnv[,varnames])
+
+#for plotting
+soilagg <- aggregate(soill)
+geodif <- gDifference(ras, soilagg, byid=TRUE)
 
 #removing rows from soil that dont exist in ras
 
