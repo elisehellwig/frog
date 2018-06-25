a86fca2950c42185858db1c94189a7811539ce61
elisehellwig <echellwig@ucdavis.edu>
2018-06-24 20:06:12 -0700

    working on soils

--- preprocess/3_ProcessSoils.R
+++ preprocess/3_ProcessSoils.R
@@ -5,6 +5,7 @@ library(raster)
 library(rgdal)
 library(rgeos)
 library(foreign)
+library(ehelpr)
 
 #setting paths
 funpath <- '/Users/echellwig/Research/frog/functions/'
@@ -22,17 +23,10 @@ ras <- readRDS(file.path(datapath, 'processed/RasiStreamLines.RDS'))
 
 soill <- spTransform(soil, crs(ras))
 
-rsv <- overChr(ras, soill, 'SoilOrderD')
+ras$soiltype <- overChr(ras, soill, 'SoilOrderD')
 
-soil <- rbind(soilca[, varnames], soilnv[,varnames])
 
 #for plotting
 soilagg <- aggregate(soill)
 geodif <- gDifference(ras, soilagg, byid=TRUE)
-
-#removing rows from soil that dont exist in ras
-
-
-RasiSoil <- merge(ras, soil, by.x='comid',by.y='NHDFlowlin')
-
 
