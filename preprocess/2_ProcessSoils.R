9bb336c7f02c2756c384256856bb37bdfd0b7b3e
elisehellwig <echellwig@ucdavis.edu>
2018-06-26 09:27:36 -0700

    processSoils is done and all the NAs have been dealt with.

--- preprocess/2_ProcessSoils.R
+++ preprocess/2_ProcessSoils.R
@@ -33,12 +33,6 @@ ras$soiltype <- overChr(ras, soill, 'SoilOrderD')
 ras$symbol <- overChr(ras, soill, 'EsriSymbol')
 ras$soilname <- overChr(ras, soill, 'muname')
 
-#Names and symbols for missing soil types 
-missingSoils <- as.data.frame(rasNAs[,c('symbol','soilname')])
-write.csv(missingSoils, file.path(datapath, 'processed/missingSoils.csv'),
-          row.names=FALSE)
-
-###########################################################
 
 #finding which rows have NAs
 soilNAs <- which(is.na(ras$soiltype))
@@ -46,15 +40,21 @@ soilNAs <- which(is.na(ras$soiltype))
 #saving the mu names and the esri symbols for help in identifying soil type
 rasNAs <- ras[soilNAs, ]
 
-#save missing soils data
-write.csv(rasNAs, file.path(datapath, 'processed/missingSoils.csv'),
-          row.names=FALSE)
+
+#Names and symbols for missing soil types 
+#missingSoils <- as.data.frame(rasNAs[,c('symbol','soilname')])
+#write.csv(missingSoils, file.path(datapath, 'processed/missingSoils.csv'),
+        #  row.names=FALSE)
+
+###########################################################
+
+
 
 #read in the df that tells us what to replace all the NAs in the soils data with
 soilkey <- read.csv(file.path(datapath, 'processed/missingSoilsKey.csv'))
 
 #doing the replacement
-ras$soiltype[soilNAs] <- soilkey$soilkey
+ras$soiltype[soilNAs] <- soilkey$class
 
 saveRDS(ras, file.path(datapath, 'processed/RasiStreamLines.RDS'))
 
