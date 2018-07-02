6b7c9be3aeb4a19b2a0ddfaab90feb593170a1c0
elisehellwig <echellwig@ucdavis.edu>
2018-07-02 13:19:59 -0700

    removed the plotting from the cleaning and added to more variables for comparison

--- preprocess/3_cleanWHR.R
+++ preprocess/3_cleanWHR.R
@@ -31,64 +31,108 @@ whrname <- c('Alpine-Dwarf Shrub','Annual Grassland','Aspen','Barren',
 
 # Merging WHR Layers ------------------------------------------------------
 
-
-
-streamTA <- spTransform(stream, TA)
-streamExt <- extent(streamTA)
-
-whrpath <- file.path(datapath, 
-                     'frog_model/data/datasets/cwhr_4Megan/CWHRVg.gdb')
-
-whrlayers <- ogrListLayers(whrpath)
-attributes(whrlayers) <- NULL
-
-#ogrListLayers
-whrlist <- lapply(whrlayers, function(l) {
-    readOGR(dsn=whrpath, layer=l, stringsAsFactors = FALSE)
-}) 
-
-whrlistTA <- lapply(whrlist, function(spdf) {
-    crop(spTransform(spdf, TA), streamExt)
-    })
-
-# whrpaths <- paste('whr', whrnames, sep='/')
-# whrlist <- lapply(whrpaths, function(fn) shapefile(file.path(datapath, fn)))
-
-whr <- do.call(bind, whrlistTA)
-
-saveRDS(whr, file.path(datapath, 'processed/whr.RDS'))
-shapefile(whr, file.path(datapath, 'processed/shapefiles/whr.shp'))
-
+# ##Only need to run this once
+# 
+# streamTA <- spTransform(stream, TA)
+# streamExt <- extent(streamTA)
+# 
+# whrpath <- file.path(datapath, 
+#                      'frog_model/data/datasets/cwhr_4Megan/CWHRVg.gdb')
+# 
+# whrlayers <- ogrListLayers(whrpath)
+# attributes(whrlayers) <- NULL
+# 
+# #ogrListLayers
+# whrlist <- lapply(whrlayers, function(l) {
+#     readOGR(dsn=whrpath, layer=l, stringsAsFactors = FALSE)
+# }) 
+# 
+# whrlistTA <- lapply(whrlist, function(spdf) {
+#     crop(spTransform(spdf, TA), streamExt)
+#     })
+# 
+# # whrpaths <- paste('whr', whrnames, sep='/')
+# # whrlist <- lapply(whrpaths, function(fn) shapefile(file.path(datapath, fn)))
+# 
+# whr <- do.call(bind, whrlistTA)
+# 
+# saveRDS(whr, file.path(datapath, 'processed/whr.RDS'))
+# shapefile(whr, file.path(datapath, 'processed/shapefiles/whr.shp'))
+# 
 
 # WHR Processing ----------------------------------------------------------
 
-
-
+#read in merged file
 whr <- readRDS(file.path(datapath, 'processed/whr.RDS'))
 whr <- spTransform(whr, crs(ras))
-
-ras$whrtype <- overChr(ras, whr, 'WHRTYPE') #70 NAs 
-ras$whrdensity <- overChr(ras, whr, 'WHRDENSITY') #70 NAs 
-ras$whrsize <- overChr(ras, whr,'WHRSIZE') #194 NAs 
-
 
 
 # WHR Type ----------------------------------------------------------------
 
 
-#All observations with NAs for whrtype have no rasi presence
+## All 70 observations with NAs for whrtype have no rasi presence
+## Merging with lifeform and covertype does not eliminate these nas
 
+ras$whrtype <- overChr(ras, whr, 'WHRTYPE') 
 brrIds <- which(ras$whrtype=='BBR')
 ras$whrtype[brrIds] <- "BAR"
+
+
+# WHR Density -------------------------------------------------------------
+
+ras$whrdensity <- overChr(ras, whr, 'WHRDENSITY') #70 NAs 
+recodeBlank(ras$whrdensity)
+
+
+# #after recoding blanks as NAs there were 13 rows with rasi presence that had
+# #WHR Density as NA
+# 
+# densNA <- which(is.na(ras$whrdensity))
+# table(ras$rasi[densNA])
+
+
+
+# WHR Size ----------------------------------------------------------------
+
+ras$whrsize <- overChr(ras, whr,'WHRSIZE') #194 NAs 
+
+ras$whrsize <- recodeBlank(ras$whrsize)
+
+####there are 10 observations with rasi presence that have have WHRsize as NA
+# sizeNA <- which(is.na(ras$whrsize))
+# table(ras$rasi[sizeNA])
+# 
+# whrNA <- union(sizeNA, densNA)
+# table(ras$rasi[whrNA])
+
+
+# Life Form ---------------------------------------------------------------
+
+ras$lifeform <- overChr(ras, whr,'WHRLIFEFORM') # 870 NAs 
+ras$lifeform <- recodeBlank(ras$lifeform)
+
+
+
+# Cover Type --------------------------------------------------------------
+
+ras$covertype <- overChr(ras, whr,'COVERTYPE') #478 NAs 
+ras$covertype <- recodeBlank(ras$covertype)
+
+
+
+# Save File ---------------------------------------------------------------
+
+saveRDS(ras, file.path('processed/RasiStreamLines2.RDS'))
+
+
+# WHR Type Plots ----------------------------------------------------------
+
 
 
 typedf <- data.frame(name=whrname,
                      whr=table(ras$whrtype))
 names(typedf) <- c('name','code','count')
 typedf$xlabel <- paste0(typedf$name, ' (', typedf$code, ')') 
-
-
-# WHR Type Plots ----------------------------------------------------------
 
 
 typehist1 <- ggplot(data=typedf) +
@@ -108,17 +152,10 @@ typehist2 <- ggplot(data=typedf[typedf$code!='SMC',]) +
     theme(axis.text.x = element_text(angle = 65, hjust = 1)) 
 
 
-# WHR Density -------------------------------------------------------------
-
-blankdens <- which(ras$whrdensity==' ' | ras$whrdensity=='')
-ras$whrdensity[blankdens] <- NA
+# WHR Density Plots ---------------------------------------------------------
 
 
-densNA <- which(is.na(ras$whrdensity))
-table(ras$rasi[densNA])
 
-#after recoding blanks as NAs there were 13 rows with rasi presence that had
-#WHR Density as NA
 
 
 densname <- c('D: 60-100% Closed','M: 40-59% Closed','P: 25-39% Closed',
@@ -131,8 +168,6 @@ names(densdf) <- c('name','code','count')
 densdf$name <- factor(densdf$name, 
                       levels(densdf$name)[c(4,3,2,1,5)])
 
-# WHR Density Plots -------------------------------------------------------
-
 
 denshist <- ggplot(data=densdf) + 
     geom_bar(aes(x=name, y=count), stat='identity') +
@@ -143,17 +178,9 @@ denshist <- ggplot(data=densdf) +
     
 
 
-# WHR Size ----------------------------------------------------------------
+# WHR Size Plots ----------------------------------------------------------
 
-blanksize <- which(ras$whrsize==' ' | ras$whrsize=='')
-ras$whrsize[blanksize] <- NA
 
-sizeNA <- which(is.na(ras$whrsize))
-#there are 10 observations with rasi presence that have have WHRsize as NA
-table(ras$rasi[sizeNA])
-
-whrNA <- union(sizeNA, densNA)
-table(ras$rasi[whrNA])
 
 #There are 15 total observations with rasi presence that have either WHRsize 
 #or whrdensity as NA
@@ -172,13 +199,29 @@ sizedf$xlabel <- paste0(sizedf$name, ' (', sizedf$DBH, ')')
 sizedf$xlabel <- fct_relevel(sizedf$xlabel, sizedf$xlabel[c(2:6,1)])
 
 
-# WHR Size Plots ----------------------------------------------------------
-
-
 sizehist <- ggplot(data=sizedf) +
     geom_bar(aes(x=xlabel, y=count), stat='identity') + 
     labs(x='Average Tree Size Class', y='Stream Reach Count', 
          title='Number of stream reaches by average DBH, n=2705') +
     theme_bw() + 
     theme(axis.text.x = element_text(angle = 40, hjust = 1)) 
+
+
+# Life Form Plot ----------------------------------------------------------
+#note I don't know what NFO stands for
+
+lifename <- c('Conifer','Hardwood','Herbaceous','Mixed','NFO','Shrub')
+lifedf <- data.frame(name=lifename,
+                     whr=table(ras$lifeform))
+names(lifedf) <- c('name','code','count')
+
+lifehist <- ggplot(data=lifedf) + 
+    geom_bar(aes(x=name, y=count), stat='identity') +
+    labs(x='WHR Life Form', y='Stream Reach Count',
+         title='Number of stream reaches by Life Form, n=2232') +
+    theme_bw(12) + 
+    theme(axis.text.x = element_text(angle = 65, hjust = 1)) 
+
+
+
 
