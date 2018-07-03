0ff97a9c4e71fb4b63a1ec353e6e36a0719dea07
elisehellwig <echellwig@ucdavis.edu>
2018-07-03 13:24:13 -0700

    added linked separations in my code. Merged in the updated rasi data.

--- preprocess/1_MergeStreams.R
+++ preprocess/1_MergeStreams.R
@@ -1,11 +1,15 @@
 #######################Data Prep######################
 ## this script imports the CA and NV data and merges it.
 
-###############Setup##############
+
+# Setup -------------------------------------------------------------------
+
+
 #loading required packages 
 library(raster)
 library(rgdal)
 library(tidyverse)
+options(stringsAsFactors = FALSE)
 
 #saving paths I use to variables for later use
 funpath <- '/Users/echellwig/Research/frog/functions/'
@@ -16,12 +20,22 @@ source(file.path(funpath, 'preprocess.R'))
 
 #importing spatial streams data
 ca <- shapefile(file.path(datapath,
-            'cathy/rasi_streams/NHDflowline_Streams_Final_withRASI.shp'))
+            'raw/rasi/NHDflowline_Streams_Final_withRASI.shp'))
 nv <- shapefile(file.path(datapath, 
-            'cathy/rasi_streams/NHDflowlineGB_Streams_Final_withRASI.shp'))
+            'raw/rasi/NHDflowlineGB_Streams_Final_withRASI.shp'))
 
-#importing cathy's data to see what we need
-#########################################################
+#importing updated absence/presence data for rasi
+rasiNV <- read.table(file.path(datapath,
+                    'raw/rasi/NHDflowlineGB_Streams_Final_RASIPres070118.tab'),
+                    sep="\t", header=TRUE)
+rasiCA <- read.table(file.path(datapath,
+                    'raw/rasi/NHDflowline_Streams_Final_RASIPres_070118.tab'),
+                     sep="\t", header=TRUE)
+
+
+# Spatial Merge ------------------------------------------------------
+
+
 #adding state variable
 ca$state <- 'CA'
 nv$state <- 'NV'
@@ -44,19 +58,41 @@ caMerge <- ca[, varnames]
 #merging the datasets with only the columns they share
 streams <- rbind(nvMerge, caMerge)
 
-names(streams) <- gsub("_prese", '', names(streams))
+
+# Cleaning Streams Data ---------------------------------------------------
+
+#removing the old rasi pa data
+streams$rasi_prese <- NULL
 
 streams$fcode <- recode(streams$fcode, `46006`='perennial', 
                         `46003`='intermittent', `46000`='other')
 
 
-#calculating stream reach length
-streams$length <- SpatialLinesLengths(streams)
+#calculating stream reach length, in km
+streams$length <- SpatialLinesLengths(streams, longlat = TRUE)
+
+
+# Non-Spatial Merge -------------------------------------------------------
+
+#converting names to lower for consistency
+names(rasiCA) <- tolower(names(rasiCA))
+names(rasiNV) <- tolower(names(rasiNV))
+
+#rbinding comid and rasi presence 
+rasi <- rbind(rasiCA[,-3], rasiNV[,-3])
+names(rasi)[2] <- 'rasi' 
+
+#merging the updated rasi p/a data with the streams data
+streamsRasi <- merge(streams, rasi, by='comid')
+
+
+
+# Saving output ---------------------------------------------------------
 
 
 #removing the column 'comid_12'
-c12_ID <- which(names(streams)=='comid_12')
-streamsfinal <- streams[,-c12_ID]
+c12_ID <- which(names(streamsRasi)=='comid_12')
+streamsfinal <- streamsRasi[,-c12_ID]
 
 
 #saving the spatiallinesdataframe to to a smaller file type
