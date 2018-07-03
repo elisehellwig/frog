b93c1a5c80c5684c1001c8009bf18762e79c2d96
elisehellwig <echellwig@ucdavis.edu>
2018-07-03 12:52:45 -0700

    commented out some code that only needs to be run once.

--- preprocess/4_ProcessDEM.R
+++ preprocess/4_ProcessDEM.R
@@ -77,13 +77,13 @@ writeRaster(streamDEM, file.path(datapath,'processed/streamGeography.grd'),
 
 # Extract Values ----------------------------------------------------------
 
-dem <- brick(file.path(datapath,'processed/streamGeography.grd'))
+#dem <- brick(file.path(datapath,'processed/streamGeography.grd'))
 
 #note this takes about 2.5 hours to run
 #demdf <- extract(dem, ras, df=TRUE)
 #saveRDS(demdf, file.path(datapath, 'processed/extractedDEMvalues.RDS'))
 
-demdf <- readRDS( file.path(datapath, 'processed/extractedDEMvalues.RDS'))
+demdf <- readRDS(file.path(datapath, 'processed/extractedDEMvalues.RDS'))
 demdf$fid <- as.factor(demdf$ID)
 
 #Calculating slope summary statistics for each reach
