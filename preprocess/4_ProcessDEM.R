aa45850ae9288482570cc7afbd7a173bbace2315
elisehellwig <echellwig@ucdavis.edu>
2018-07-02 17:03:03 -0700

    finally got the cardinal direction data converted to strings!

--- preprocess/4_ProcessDEM.R
+++ preprocess/4_ProcessDEM.R
@@ -21,40 +21,41 @@ funpath <- '/Users/echellwig/Research/frog/functions/'
 source(file.path(funpath, 'preprocess.R'))
 
 #change to RasiStreamLines3 when that gets created
-ras <- readRDS(file.path(datapath, 'processed/RasiStreamLines2.RDS'))
+ras <- readRDS(file.path(datapath, 'processed/RasiStreamLines3.RDS'))
 
 
 
 # Create DEM --------------------------------------------------------------
-
-#creating file names of the DEMS
-grids <- paste0('n', rep(c(40,41), each=3), 'w', rep(120:122, 2))
-fnames <- paste0('float', grids, '_13.flt')
-
-#modifying the third one because USGS can't seem to be consistent in their
-#file structure
-grids[3] <- 'USGS_NED_13_n40w122_GridFloat'
-fnames[3] <- 'usgs_ned_13_n40w122_gridfloat.flt'
-
-
-#loading the DEM tiles into R in list form 
-demlist <- lapply(seq_along(fnames), function(i) {
-    fn <- file.path(datapath,'DEM', grids[i], fnames[i])
-    raster(fn)
-})
-
-#merging the dem tiles together into one object
-dem <- do.call(merge, demlist)
-
-#Creating a bounding box to use to crop the DEM object
-streamExt <- extent(-121.7, -120, 39.2, 40.4)
-
-#croping the object so we only have the DEM where we have streams
-streamDEM <- crop(dem, streamExt)
-
-#saving the DEM as a GRD
-writeRaster(streamDEM, file.path(datapath, 'processed/localDEM.grd'))
-
+# ## only need to do this once
+# 
+# #creating file names of the DEMS
+# grids <- paste0('n', rep(c(40,41), each=3), 'w', rep(120:122, 2))
+# fnames <- paste0('float', grids, '_13.flt')
+# 
+# #modifying the third one because USGS can't seem to be consistent in their
+# #file structure
+# grids[3] <- 'USGS_NED_13_n40w122_GridFloat'
+# fnames[3] <- 'usgs_ned_13_n40w122_gridfloat.flt'
+# 
+# 
+# #loading the DEM tiles into R in list form 
+# demlist <- lapply(seq_along(fnames), function(i) {
+#     fn <- file.path(datapath,'DEM', grids[i], fnames[i])
+#     raster(fn)
+# })
+# 
+# #merging the dem tiles together into one object
+# dem <- do.call(merge, demlist)
+# 
+# #Creating a bounding box to use to crop the DEM object
+# streamExt <- extent(-121.7, -120, 39.2, 40.4)
+# 
+# #croping the object so we only have the DEM where we have streams
+# streamDEM <- crop(dem, streamExt)
+# 
+# #saving the DEM as a GRD
+# writeRaster(streamDEM, file.path(datapath, 'processed/localDEM.grd'))
+# 
 
 # Process DEM -------------------------------------------------------------
 
@@ -76,4 +77,43 @@ writeRaster(streamDEM, file.path(datapath,'processed/streamGeography.grd'),
 
 # Extract Values ----------------------------------------------------------
 
+dem <- brick(file.path(datapath,'processed/streamGeography.grd'))
+
+#note this takes about 2.5 hours to run
+demdf <- extract(dem, ras, df=TRUE)
+
+demdf$fid <- as.factor(demdf$ID)
+
+#Calculating slope summary statistics for each reach
+ras$slopemax <- tapply(demdf$slope, demdf$fid, FUN = max)
+ras$slopemin <- tapply(demdf$slope, demdf$fid, FUN = min)
+ras$slopemean <- tapply(demdf$slope, demdf$fid, FUN = mean)
+
+#elevation 
+ras$elevmax <- tapply(demdf$elevation, demdf$fid, FUN = max)
+
+
+# Process Aspect ----------------------------------------------------------
+
+#create aspect key
+
+## aspect values
+vals <- c(0, rep(seq(22.5, 337.5, by=45), 2), 360)
+aspdf <- as.data.frame(matrix(vals, ncol=2))
+
+names(aspdf) <- c('min', 'max')
+
+#IDs 
+aspdf$id <- 1:nrow(aspdf)
+
+#cardinal directions
+aspdf$string <- c('N', 'NE', 'E', "SE",'S','SW','W','NW', 'N')
+
+
+ras$cardinal <- sapply(1:length(ras), function(i) {
+    v <- demdf[demdf$ID==i, 'aspect']
+    vcard <- recodeRange(v, aspdf)
+    getMode(vcard)
+    
+})
 
