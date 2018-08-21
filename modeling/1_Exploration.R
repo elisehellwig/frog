b91176383a993c8c52bb2f7fed6f0eebbb7a2ec8
elisehellwig <echellwig@ucdavis.edu>
2018-08-21 13:02:59 -0700

    more exploration

--- modeling/1_Exploration.R
+++ modeling/1_Exploration.R
@@ -1,6 +1,7 @@
 library(raster)
 library(rgdal)
 library(randomForest)
+library(plyr)
 library(dplyr)
 library(dismo)
 library(DMwR)
@@ -11,7 +12,7 @@ funpath <- '/Users/echellwig/Research/frog/functions/'
 source(file.path(funpath, 'fitting.R'))
 source(file.path(funpath, 'modeval.R'))
 
-rasiSP <- readRDS(file.path(datapath, 'processed/RasiStreamLinesFinal.RDS'))
+#rasiSP <- readRDS(file.path(datapath, 'processed/RasiStreamLinesFinal.RDS'))
 rasi <- read.csv(file.path(datapath,'processed/RasiStreamDF.csv'))
 
 
@@ -21,51 +22,87 @@ rasi <- read.csv(file.path(datapath,'processed/RasiStreamDF.csv'))
 
 rasi5000 <- rasi[rasi$elevmax>1524, ]
 
-fullvars <- c('rasi', 'length','elevmax', 'elevmin','slopemin', 
-              'slopemax','slopemean',
-              'totDrainArea', 'divDrainArea','south','perennial', 'x','y',
-              'hard', paste0('bio', 1:19))
+fullids <- c(3:23, 104:122)
+fullvars <- names(rasi5000)[fullids]
 
+rasifull <- rasi5000[,fullvars]
 
-rasirf <- rasi5000[,fullvars]
+rasifull$seasonality <- NULL
+rasifull$south <- NULL
+rasifull$hard <- NULL
+#rasifull$perennial <- as.factor(rasifull$perennial)
+rasifull$rasi <- as.factor(rasifull$rasi)
 
 
 set.seed(203943)
-fold <- kfold(rasirf, 5, by=rasirf$rasi)
-test <- rasirf[fold==1, ]
-train <- rasirf[fold!=1, ]
+fold <- kfold(rasifull, 5, by=rasifull$rasi)
+fulldf <- expandFactors(rasifull, 'rasi')
+fulldf2 <- convertFactors(fulldf, varnames= c('cardinal','bedrock','soil',
+                                                 'habitat','rocktype'))
+names(fulldf2)[56] <- 'waterrocktype'
 
-mefull <- maxent(x=train[,-1], p=train$rasi)
-fullpred <- predictPres(mefull, test, prob=0.7)
+test <- fulldf2[fold==1, ]
+train <- fulldf2[fold!=1, ]
+
+mefull <- maxent(x=fulldf2[,-1], p=fulldf2$rasi)
+
+allvars <- names(fulldf2)[-1]
+
+for (v in allvars) {
+    print(v)
+    filename <- paste0(v, '.png')
+    png(file.path(datapath, 'results/plots/response', filename))
+        response(mefull, var=v)
+    dev.off()
+}
+
+
+fullpred <- predictPres(mefull, test, prob=0.5)
 
 metric(fullpred, test$rasi, type='PPV')
 metric(fullpred, test$rasi, type='NPV')
 
 
-impOrd <- order(importance(rffull), decreasing=TRUE)
-importance(rffull)[impOrd,]
+drop1 <- c('NW', 'NE','bio17','bio13', 'bio18','bio11','bio10', 'bio16',
+           'bio1','bio9','SW','CON','Andisols', 'W','S','NE','waterrocktype',
+           'igneous','metamorphic', 'sedimentary','elevmax','NoSoil', 'bio19',
+           'divDrainArea', 'J','S','Tr', 'Qoa','Pm','Ti','gb', 'm','grMz',
+           'Ec','C','SO', 'waterrocktype')
+rasi1 <- fulldf2 %>% select(-drop1)
 
-
-vars1 <- c('rasi', 'y','x', 'length','divDrainArea','bio8','slopemin',
-           'elevmin','bio13','bio6','bio11')
 rasi1 <- rasirf[,vars1]
 train1 <- rasi1[fold!=1, ]
 test1 <- rasi1[fold==1,]
 
-me1 <- maxent(x=train1[,-1], p=train1$rasi)
+me1 <- maxent(x=rasi1[,-1], p=rasi1$rasi)
+
+allvars1 <- names(rasi1)[-1]
+
+for (v in allvars1) {
+    print(v)
+    filename <- paste0(v, '.png')
+    png(file.path(datapath, 'results/plots/response', filename))
+    response(mefull, var=v)
+    dev.off()
+}
+
+
 pred1 <- predictPres(me1, test1, prob=0.65)
 metric(pred1, test1$rasi, type='PPV')
 
 
 
 # SMOTE Maxent attempt --------------------------------------------------------
+##Note maxent() only works with two level factors
 
-factorvars <- c('rasi','hard','south','perennial')
+fids <- which(sapply(1:ncol(rasirf), function(i) (!is.numeric(rasirf[, i]))))
 rasif <- rasirf
 
-for (var in factorvars) {
-    rasif[, var] <- as.factor(rasif[,var])
+for (i in fids) {
+    rasif[, i] <- as.factor(rasif[,i])
 }
+
+rasif[,'south'] <- as.factor(rasif[,'south'])
 
 set.seed(192838)
 
