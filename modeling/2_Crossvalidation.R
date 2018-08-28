1d7fc1f20fc68e3861b2f4f6fd24d2cdbcb4af83
elisehellwig <echellwig@ucdavis.edu>
2018-08-28 09:21:58 -0700

    run and crossvalidate 3 models with decreasing numbers of variables

--- modeling/2_Crossvalidation.R
+++ modeling/2_Crossvalidation.R
@@ -14,33 +14,30 @@ source(file.path(funpath, 'fitting.R'))
 source(file.path(funpath, 'modeval.R'))
 
 
-rasiSP <- readRDS(file.path(datapath, 'processed/RasiStreamLinesFinal.RDS'))
-rasi <- read.csv(file.path(datapath,'processed/RasiStreamDF.csv'))
-
-#data is imbalanced at a ratio of 40:1
-
-vars1 <- c('rasi', 'length','elevmax', 'elevmin','slopemin', 'slopemax', 
-           'slopemean','totDrainArea', 'divDrainArea', 'south','perennial',
-           'x','y', 'hard', paste0('bio', 1:19))
-dropvars <- c('bio1', 'bio7', 'bio10','bio16','bio17','bio18','bio19')
+#rasiSP <- readRDS(file.path(datapath, 'processed/RasiStreamLinesFinal.RDS'))
+rasi <- read.csv(file.path(datapath,'processed/RasiModelDF.csv'))
+dropvars1 <- readRDS(file.path(datapath, 'processed/uselessVariables.RDS'))
+dropvars2 <- readRDS(file.path(datapath, 'processed/uselessVariables2.RDS'))
+dropvars3 <- readRDS(file.path(datapath, 'processed/uselessVariables3.RDS'))
 
 
-varsla <- c('rasi', 'length','elevmax', 'elevmin','slopemin', 'slopemax', 
-            'slopemean','totDrainArea', 'divDrainArea', 'south','perennial',
-            'x','y', 'hard', paste0('bio', 1:19), 'rocktype')
+rasi <- convertFactors(rasi)
+rasi$rasi <- factor(rasi$rasi)
 
 
 
 #subsetting data to be only some variables and no obs below 1524m (5000ft)
-rasi1 <- rasi[rasi$elevmax>=1524, vars1] 
-rasi2 <- rasi1 %>% select(-dropvars)
-rasila <- rasi[rasi$elevmax>=1524, varsla] 
-rasila$rocktype <- factor(rasila$rocktype)
+rasi1 <- rasi %>% 
+    select(-dropvars1) %>% 
+    filter(elevmax>=1524)
 
-rasi1$rasi <- factor(rasi1$rasi)
-rasi2$rasi <- factor(rasi2$rasi)
+rasi2 <- rasi1 %>% select(-dropvars2)
+rasi3 <- rasi2 %>% select(-dropvars3)
 
-la2 <- maxent(rasi2[,-1], rasi2$rasi, args=c("defaultprevalence=0.73"))
+mod1 <- maxent(rasi1[,-1], rasi1$rasi, args=c("defaultprevalence=0.73"))
+mod2 <- maxent(rasi2[,-1], rasi2$rasi, args=c("defaultprevalence=0.73"))
+mod3 <- maxent(rasi3[,-1], rasi3$rasi, args=c("defaultprevalence=0.73",
+                                              "responsecurves=true"))
 
 
 # Cross-Validating Sans Smote -------------------------------------------------
@@ -64,15 +61,31 @@ cvdf2 <- data.frame(threshold=thresholds,
                     PPV=round(cvthresh2[,1],3),
                     TruePos=round(cvthresh2[,2]))
 
-# Crossvalidating with SMOTE ----------------------------------------------
 
-
-
-cvthreshsmote1 <- t(sapply(thresholds, function(th) {
-    crossval(rasi1, seed=928191, threshold=th, errormetric=c('PPV','TruePos'),
-             smote=TRUE)
+cvthresh3 <- t(sapply(thresholds, function(th) {
+    crossval(rasi3, seed=928191, threshold=th, errormetric=c('PPV','TruePos'))
 }))
 
-cvdfsmote1 <- data.frame(threshold=thresholds,
-                    PPV=round(cvthreshsmote1[,1],3),
-                    TruePos=round(cvthreshsmote1[,2]))
+cvdf3 <- data.frame(threshold=thresholds,
+                    PPV=round(cvthresh3[,1],3),
+                    TruePos=round(cvthresh3[,2]))
+
+
+write.csv(cvdf1, file.path(datapath, 'results/CrossValidationModel51.csv'),
+          row.names = FALSE)
+write.csv(cvdf2, file.path(datapath, 'results/CrossValidationModel22.csv'),
+          row.names = FALSE)
+write.csv(cvdf3, file.path(datapath,'results/CrossValidationModel15.csv'),
+          row.names=FALSE)
+
+# Response Plots ----------------------------------------------
+
+allvars22 <- names(rasi2)[-1]
+
+for (v in allvars22) {
+    filename <- paste0(v, '.png')
+    png(file.path(datapath, 'results/plots/response22', filename))
+    response(mod2, var=v)
+    dev.off()
+}
+
