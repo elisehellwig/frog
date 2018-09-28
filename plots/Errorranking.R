74bc5ed9bf89aca4803224c19fdd29f1532153d8
elisehellwig <echellwig@ucdavis.edu>
2018-09-28 12:21:23 -0700

    creates the errorcurve plot

--- plots/Errorranking.R
+++ plots/Errorranking.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(ggplot2)
 library(reshape2)
 library(RColorBrewer)
@@ -5,20 +6,26 @@ library(RColorBrewer)
 datapath <- '/Volumes/GoogleDrive/My Drive/OtherPeople/frogData/data'
 cv <- read.csv(file.path(datapath, 'results/CrossValidationModelFinal.csv'))
 
+cv$TruePos <- cv$TruePos/15
+
+cv <- select(cv, -omission)
+
 cvm <- melt(cv, id.vars = 'threshold',variable.name = 'metric')
-levels(cvm$metric) <- c('Positive Predictive Value','Number of True Positives')
+levels(cvm$metric) <- c('Positive Predictive Value',
+                        'True Positive Rate')
 
 
 pal <- colorRampPalette(brewer.pal(9, 'YlGnBu'))
 
+#convert true positive to percent
 p <- ggplot(data=cvm) +
     geom_line(aes(x=threshold, y=value)) +
-    facet_wrap(~metric, scales='free') +
-    labs(x='Probability Threshold',
+    facet_wrap(~metric) +
+    labs(x='Probability of Occupancy Threshold',
          y='Metric Value') +
-    theme_bw(10) 
+    theme_bw(6) 
 
-tiff(file.path(datapath, 'results/plots/ErrorCurve.tiff'), width = 600,
-     res=150)
+tiff(file.path(datapath, 'results/plots/ErrorCurve.tiff'), width = 900,
+     res=300)
     p
 dev.off()
