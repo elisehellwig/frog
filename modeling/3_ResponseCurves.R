d31b26e24e0a9299deecf1d4479059076b483993
elisehellwig <echellwig@ucdavis.edu>
2018-09-28 12:21:04 -0700

    removed all the old code that doesn't work with the bootstrapped values from Maxent function

--- modeling/3_ResponseCurves.R
+++ modeling/3_ResponseCurves.R
@@ -81,102 +81,47 @@ rci2 <- rci[-badlengths,]
 
 rm <- merge(rci2, m, by=c('variable','value'))
 
+levels(rm$variable) <- c('Disturbed Soil','Bio 11: Mean Winter Temp',
+                         'Dense Canopy Cover','Hardwood Forest',
+                         'Stream Length', 'Number of Meadows',
+                         'Mean Gradient','Stream Order',
+                         'Total Drainage Area (sqrt)', 
+                         'Number of Waterbodies (sqrt)',
+                         'Longitude')
 
+numlevs <- levels(rm$variable)[c(2, 5:11)]
+chrlevs <- levels(rm$variable)[c(1,3:4)]
 # make plots --------------------------------------------------------------
 
 
-rnum <- rm[rm$variable %in% numvars, ]
-rchr <- rm[rm$variable %in% chrvars, ]
+rnum <- rm[rm$variable %in% numlevs, ]
+rchr <- rm[rm$variable %in% chrlevs, ]
+rchr$value <- c('Not Dense','Dense', 'Not Disturbed',
+                   'Disturbed','Not Hardwood','Hardwood')
+
+numplot <- ggplot(data=rnum) + 
+    geom_line(aes(x=value, y=prob)) +
+    geom_ribbon(aes(x=value, ymax=upper, ymin=lower), alpha=0.4) +
+    facet_wrap(~variable, scales='free_x') + 
+    ylim(c(0,1)) + 
+    theme_bw(10) + 
+    labs(x='Variable Value',y='Probability of Occupancy')
+    
+
+ggsave(file.path(datapath,'results/plots/response/NumericResponse.tiff'),
+       plot=numplot)
 
 
+chrplot <- ggplot(data=rchr) +
+    geom_bar(aes(x=value,y=prob), stat='identity') +
+    geom_errorbar(aes(x=value, ymin=lower, ymax=upper), width=0.2) +
+    facet_wrap(~variable, scales='free_x') +
+    ylim(low=0, high=1) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    theme_bw(10) +
+    labs(y='Probability of Occupancy', x='') 
+    
+ggsave(file.path(datapath,'results/plots/response/CategoricalResponse.tiff'),
+       plot=chrplot)
 
 
-#rasiSP <- readRDS(file.path(datapath, 'processed/RasiStreamLinesFinal.RDS'))
-r0 <- readRDS(file.path(datapath,'results/rasiplot.RDS'))
-vars <- read.csv(file.path(datapath, 'results/SelectedVariables.csv'))
-vars <- as.character(vars$x)
-
-rr <- r0[, vars] 
-
-rasi0 <- convertFactors(rasi0, exclude='rasi')
-r <- convertFactors(rr, exclude='rasi')
-
-#subsetting data to be only some variables 
-rasi <- rasi0 %>% select(vars) 
-rasi$bio11 <- rasi$bio11/10
-
-# Running the model -------------------------------------------------------
-
-
-
-
-# Response curves ---------------------------------------------------------
-
- numvars <- vars[c(3,6:12)]
- chrvars <- vars[c(2,4,5)]
- 
- rcsNumeric <- ldply(numvars, function(var) {
-     d <- data.frame(response(mod, var, expand=0, at=median))
-     d$variable <- var
-     names(d)[1:2] <- c('value','response')
-     d
- })
-
- 
- 
- # 
-# write.csv(rcsNumeric, file.path(datapath, 'results/responseNum.csv'),
-#           row.names = FALSE)
-# 
-# rcsChar <- ldply(chrvars, function(var) {
-#     d <- responseCurve(rasi, var, 50, 30, 23882929, plot=FALSE)
-#     d$variable <- var
-#     d
-# })
-# 
-# write.csv(rcsChar, file.path(datapath, 'results/responseChr.csv'),
-#           row.names = FALSE)
-
-
-la <- responseCurve(rasi, 'waterbodies', 1000, 30, 23882929)
-
-
-nplot <- ggplot(data=rcsNumeric) +
-    geom_line(aes(x=value, y=response)) +
-    facet_wrap(~variable, scales='free') +
-    scale_y_continuous(limits = c(0, 1))
-
-rn <- read.csv( file.path(datapath, 'results/responseNum.csv'))
-rc <- read.csv( file.path(datapath, 'results/responseChr.csv'))
-
-rn$valueScaled <- as.numeric(scale_by(value ~ variable, data=rn))
-
-nplot <- ggplot(data=rn) + 
-    geom_line(aes(x=valueScaled, y=response)) +
-    facet_wrap( ~ variable)
-
-nplot <- ggplot(data=rn[rn$variable=='waterbodies', ]) + 
-    geom_line(aes(x=(value)^2, y=response)) 
-
-rcs$value <- as.numeric(rcs$value)
-
-v <- responseCurve(rasi, 'waterbodies', 10, 10, 23882929, plot=FALSE)
-
-p <- responseCurve(rasi, 'waterbodies', 100, 30, 23882929)
-
-pm <- p + labs(x='Waterbodies', y='Predicted Presence Probability') + 
-    theme_bw() + scale_y_continuous(limits=c(0,1))
-
-
-allvars <- names(rasi[,-1])
-
-for (v in allvars) {
-    print(v)
-    p <- responseCurve(rasi, v, 100, 30, 8752)
-    pm <- p + labs(x=v, y='Predicted Presence Probability') + 
-        theme_bw() + scale_y_continuous(limits=c(0,1))
-    filename <- paste0(v, '.png')
-    png(file.path(datapath, 'results/plots/newresponse', filename))
-    print(pm)
-    dev.off()
-}
