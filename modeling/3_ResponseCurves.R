4d713161b223ed8bc5b1bfa0a02f15d6a0039675
elisehellwig <echellwig@ucdavis.edu>
2018-09-28 10:39:54 -0700

    writing the code to plot bootstrapped confidence intervals on response curves

--- modeling/3_ResponseCurves.R
+++ modeling/3_ResponseCurves.R
@@ -3,7 +3,6 @@ library(dismo)
 library(plyr)
 library(dplyr)
 library(ggplot2)
-library(standardize)
 datapath <- '/Volumes/GoogleDrive/My Drive/OtherPeople/frogData/data'
 funpath <- '/Users/echellwig/Research/frog/functions/'
 
@@ -11,10 +10,88 @@ source(file.path(funpath, 'fitting.R'))
 source(file.path(funpath, 'modeval.R'))
 source(file.path(funpath, 'response.R'))
 
+rasi0 <- read.csv(file.path(datapath,'processed/RasiModelDF.csv'))
+rasi0$bio11 <- rasi0$bio11/10
+
+
+vars <- read.csv(file.path(datapath, 'results/SelectedVariables.csv'))
+vars <- as.character(vars$x)
+
+numvars <- c('bio11','length','meadows','slopemean','streamOrder',
+             'totDrainArea','waterbodies','x')
+
+chrvars <- c('Disturbed','dense','hardwood')
+
+# Import data -------------------------------------------------------------
+
+filelist <- lapply(vars[-1], function(v) {
+    list.files(path=file.path(datapath,'maxent/bootstrap1000'),
+               pattern=v, full.names = TRUE)
+})
+
+rd <- lapply(filelist, function(files) {
+    ldply(files, function(f) {
+        read.csv(f, stringsAsFactors = FALSE)
+    })
+})
+
+modfiles <- sapply(vars[-1], function(v) {
+    name <- paste0(v, '.dat')
+    list.files(path=file.path(datapath,'maxent/FullModel/plots'),
+               pattern=name, full.names = TRUE)
+})
+
+
+md <- lapply(modfiles, function(f) read.csv(f, stringsAsFactors = FALSE))
+
+# Subset Data -------------------------------------------------------------
+
+
+
+ranges <- sapply(vars[-1], function(v) {
+    range(rasi0[,v])
+})
+
+r <- lapply(1:length(rd), function(i) {
+    ids <- which(rd[[i]][,'x'] >= ranges[1,i] & rd[[i]][,'x'] <= ranges[2,i])
+    rd[[i]][ids,]
+})
+
+m <- ldply(1:length(md), function(i) {
+    ids <- which(md[[i]][,'x'] >= ranges[1,i] & md[[i]][,'x'] <= ranges[2,i])
+    md[[i]][ids,]
+})
+
+names(m)[2:3] <- c('value','prob')
+
+
+lapply(1:11, function(i) {
+    unique(table(r[[i]]$x))
+})
+
+# Calculate confidence intervals ------------------------------------------
+
+rci <- ldply(1:length(r), function(i) bootCI(r[[i]], varname=vars[i+1]))
+
+missingLengths <- setdiff(rci[rci$variable=='length','value'],
+                          m[m$variable=='length','value'])
+
+badlengths <- which(rci$variable=='length' & rci$value %in% missingLengths)
+rci2 <- rci[-badlengths,]
+
+rm <- merge(rci2, m, by=c('variable','value'))
+
+
+# make plots --------------------------------------------------------------
+
+
+rnum <- rm[rm$variable %in% numvars, ]
+rchr <- rm[rm$variable %in% chrvars, ]
+
+
 
 
 #rasiSP <- readRDS(file.path(datapath, 'processed/RasiStreamLinesFinal.RDS'))
-rasi0 <- read.csv(file.path(datapath,'processed/RasiModelDF.csv'))
 r0 <- readRDS(file.path(datapath,'results/rasiplot.RDS'))
 vars <- read.csv(file.path(datapath, 'results/SelectedVariables.csv'))
 vars <- as.character(vars$x)
