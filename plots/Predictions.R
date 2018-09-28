761e70c4bd758c3571ba7a50ac098d9a474ffbc5
elisehellwig <echellwig@ucdavis.edu>
2018-09-28 12:21:48 -0700

    updated script to use tmap package instead of base plot or ggmap

--- plots/Predictions.R
+++ plots/Predictions.R
@@ -1,16 +1,35 @@
+
+# Setup -------------------------------------------------------------------
 library(raster)
 library(rgdal)
+library(spatialEco)
 library(RColorBrewer)
 library(ggmap)
 library(tmap)
 
 datapath <- '/Volumes/GoogleDrive/My Drive/OtherPeople/frogData/data'
 
-frst <- readOGR()
+frst <- readOGR(dsn = file.path(datapath, 
+                                'raw/forest/S_USA.AdministrativeForest.shp'))
+bound <- readOGR(dsn = file.path(datapath,
+                                 'raw/rasi/boundary/StudyBoundaryFinal.shp'))
+
 fp <- readRDS(file.path(datapath, 'results/forestpolygons.RDS'))
 r0 <- readRDS(file.path(datapath,'results/rasiplot.RDS'))
+rr <- read.csv(file.path(datapath, 'results/RasiResultsDF.csv'))
 
-r <- r0[, c('id','rasi','prob','predicted')]
+fnames <- c('Lassen National Forest','Plumas National Forest',
+            'Tahoe National Forest')
+
+probpal <- colorRampPalette(brewer.pal(7, 'YlOrRd'))
+
+
+# Data Processing ---------------------------------------------------------
+rrr <- merge(r0, rr[, c('id','prob','predicted')], by='id')
+
+
+
+r <- rrr[, c('id','rasi','prob.y','predicted.y')]
 names(r)[3] <- 'Probability'
 names(fp) <- 'Forest'
 
@@ -18,8 +37,17 @@ rpres <- r[r$rasi==1, ]
 rabs <- r[r$rasi==0, ]
 forestcols <- c('grey70','grey55', 'grey40')
 
+f <- frst[frst$FORESTNAME %in% fnames,]
+f$Forest <- c("Plumas", "Lassen","Tahoe")
+fta <- spTransform(f, crs(r))
+bta <- spTransform(bound, crs(r))
 
+bb <- extent(c(-165600, -200, 140000, 267200))
+bb2 <- extent(c(-165700 , -454, 137000, 259000))
 
+fta2 <- crop(fta, bb2)
+fta3 <- remove.holes(remove.holes(fta2))
+fta3$Forest <- c("Plumas", "Lassen","Tahoe")
 # Creating plot data ------------------------------------------------------
 
 
@@ -52,51 +80,16 @@ forestcols <- c('grey70','grey55', 'grey40')
 # 
 
 
-# base plotting -----------------------------------------------------------
-
-
-
-probpal <- colorRampPalette(brewer.pal(7, 'YlOrRd'))
-
-
-r$probcol <-  probpal(50)[as.numeric(cut(r$prob, breaks = 50))]
-
-
-
-png(file.path(datapath, 'results/plots/occupancy/occupied.png'), 
-    width=1200, height=1200, res=400)
-    plot(fp, col=forestcols, border=forestcols,
-        main='R. Sierrae Occupied Streams')
-    plot(rpres, col=rpres$probcol, lwd=1, add=TRUE)
-    plot(rabs, col='black', lwd=0.5, add=TRUE)
-dev.off()
-
-png(file.path(datapath, 'results/plots/occupancy/empty.png'), 
-    width=1200, height=1200, res=400)
-    plot(fp, col=forestcols, border=forestcols,
-        main='R. Sierrae Unoccupied Streams')
-    plot(rabs, col=rabs$probcol, lwd=0.5, add=TRUE)
-    plot(rpres, col='black', lwd=0.5, add=TRUE)
-dev.off()
-
-png(file.path(datapath, 'results/plots/occupancy/allprobabilities.png'), 
-    width=1200, height=1200, res=400)
-    plot(fp, col=forestcols, border=forestcols,
-     main='R. Sierrae Streams')
-    plot(r, col=r$probcol, lwd=0.5, add=TRUE)
-dev.off()
-
-
-
 # tmap -------------------------------------------------------------------
 
 
-bb <- extent(c(-165600, -200, 140000, 266200))
 
-allprob <- tm_shape(fp, bbox=bb, is.master = TRUE) + 
+allprob <- tm_shape(fta3, bbox=bb, is.master = TRUE) + 
     tm_fill(col='Forest', palette=forestcols, title='National Forest') +
     tm_shape(r) + 
     tm_lines(col='Probability', lwd=1.5, palette = probpal(10)) +
+    tm_shape(bta) +
+    tm_borders(col='black', lwd=3) +
     tm_layout(title='Probability of Stream Occupancy for R. sierrae',
               legend.bg.color="grey87", legend.width = -0.2)
 
@@ -106,12 +99,14 @@ save_tmap(allprob, filename = allfn)
 
 bbp <- extent(c(-155600, -200, 140000, 266200))
 
-pres <- tm_shape(fp, bbox=bb, is.master = TRUE) + 
+pres <- tm_shape(fta3, bbox=bb, is.master = TRUE) + 
     tm_fill(col='Forest', palette=forestcols, title='National Forest') +
     tm_shape(rpres) + 
     tm_lines(col='Probability', lwd=1.5, palette = probpal(10)) +
     tm_shape(rabs) + 
     tm_lines(col='black') +
+    tm_shape(bta) +
+    tm_borders(col='black', lwd=3) +
     tm_layout(title='Probability of Stream Occupancy for Occupied Streams',
               legend.bg.color="grey87", legend.width = -0.2)
 
@@ -119,12 +114,14 @@ presfn <- file.path(datapath, 'results/plots/occupancy/occupied.tiff')
 save_tmap(pres, filename = presfn)
 
 
-absplot <- tm_shape(fp, bbox=bb, is.master = TRUE) + 
+absplot <- tm_shape(fta3, bbox=bb, is.master = TRUE) + 
     tm_fill(col='Forest', palette=forestcols, title='National Forest') +
     tm_shape(rabs) + 
     tm_lines(col='Probability', lwd=1.5, palette = probpal(10)) +
     tm_shape(rpres) + 
     tm_lines(col='black') +
+    tm_shape(bta) +
+    tm_borders(col='black', lwd=3) +
     tm_layout(title='Probability of Stream Occupancy for Unccupied Streams',
               legend.bg.color="grey87", legend.width = -0.2)
 
